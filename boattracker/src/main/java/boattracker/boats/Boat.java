4a1cfa29807e90071f2357be76546d3d84642b1d
Aamir Irfan <irfan@uoguelph.ca>
2023-11-06 10:49:28 -0500

    Separated into internal and boats packages

---
+++ boattracker/src/main/java/boattracker/boats/Boat.java
@@ -0,0 +1,23 @@
+package boattracker.boats;
+import boattracker.internal.*;
+import java.util.ArrayList;
+
+public class Boat {
+
+    private String modelNumber;
+    private String boatColor;
+    private double boatPrice = 0.0;
+    private ArrayList<Employee> users = new ArrayList<Employee>();
+
+    public Boat(String modelNumber){
+        this.modelNumber = modelNumber;
+    }
+
+    public String getBoatColor() {
+        return boatColor;
+    }
+
+    public double getBoatPrice() {
+        return boatPrice;
+    }
+}
---
+++ boattracker/src/main/java/boattracker/internal/Employee.java
@@ -0,0 +1,27 @@
+package boattracker.internal;
+import boattracker.boats.*;
+import java.util.ArrayList;
+
+public class Employee {
+    private int empId;
+    private String department;
+    private String name;
+    private ArrayList<Boat> boats = new ArrayList<Boat>();
+
+    public Employee(int employeeId, String department) {
+        this.empId = employeeId;
+        this.department = department;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDepartment() {
+        return this.department;
+    }
+}
